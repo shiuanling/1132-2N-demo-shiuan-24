138dbc267cafdb2423d96d7161dc950c966d6bec
shiuanling <lovelily530@gmail.com>
2025-04-30 19:15:15 +0800

    add w10

---
+++ demo/w10-prostore-24/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components//ui/button';
+
+const HomePage_24 = () => {
+  return (
+    <div>
+      <Button variant='outline'>shiuan</Button>
+      <Button variant='default'>shiuan</Button>
+    </div>
+  )
+}
+
+export default HomePage_24
